9bd4dc97fe0274f5ef1ebc2d10314a0faca1d618
Markus Schweitzer <me@markusschweitzer.at>
2016-06-20 08:04:31 +0200

    Removed unused usings.

--- PersistantStorage/PersistantList.cs
+++ PersistantStorage/PersistantList.cs
@@ -1,14 +1,8 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Bson.Serialization.IdGenerators;
-using MongoDB.Driver;
+﻿using MongoDB.Driver;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PersistantStorage
 {
