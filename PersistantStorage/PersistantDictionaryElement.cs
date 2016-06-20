0800736aa246caa823ac8cced206ffd73d007221
Markus Schweitzer <me@markusschweitzer.at>
2016-06-20 17:09:33 +0200

    Added PersistantDictionaryElement.

---
+++ PersistantStorage/PersistantDictionaryElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistantStorage
+{
+    public class PersistantDictionaryElement<K, T> : PersistantListElement<T>
+    {
+        public K KeyObject;
+
+        public PersistantDictionaryElement(K key, T item) : base(item)
+        {
+            KeyObject = key;
+        }
+
+    }
+}
