d488bff96009dea782f1a167ae4409e962ad9b09
Markus Schweitzer <me@markusschweitzer.at>
2016-06-20 08:25:55 +0200

    Added persistant storage connection.

---
+++ PersistantStorage/PersistantStorageConnection.cs
@@ -0,0 +1,68 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistantStorage
+{
+    public class PersistantStorageConnection
+    {
+        private MongoClient _client;
+
+        public bool IsConnected
+        {
+            get
+            {
+                return _client != null;
+            }
+        }
+
+        public bool Connect(string connectionString)
+        {
+            try
+            {
+                _client = new MongoClient(connectionString);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _client = null;
+                return false;
+            }
+        }
+
+        public void Disconnect()
+        {
+            _client = null;
+        }
+
+        public IMongoDatabase GetDatabase(string database)
+        {
+            if (IsConnected)
+            {
+                return _client.GetDatabase(database);
+            }
+            throw new InvalidOperationException("Client not connected!");
+        }
+
+        public IMongoCollection<T> GetCollection<T>(IMongoDatabase database, string collection)
+        {
+            if (IsConnected)
+            {
+                return database.GetCollection<T>(collection);
+            }
+            throw new InvalidOperationException("Client not connected!");
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string database, string collection)
+        {
+            if (IsConnected)
+            {
+                return GetDatabase(database).GetCollection<T>(collection);
+            }
+            throw new InvalidOperationException("Client not connected!");
+        }
+    }
+}
