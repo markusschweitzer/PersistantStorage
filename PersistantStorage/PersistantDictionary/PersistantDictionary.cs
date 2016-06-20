2fb26c1786384ce995110002f22ea6189096a6e6
Markus Schweitzer <me@markusschweitzer.at>
2016-06-20 22:04:39 +0200

    Added containskey and containsvalue method

--- PersistantStorage/PersistantDictionary/PersistantDictionary.cs
+++ PersistantStorage/PersistantDictionary/PersistantDictionary.cs
@@ -68,7 +68,17 @@ namespace PersistantStorage
             }
             return default(T);
         }
-        
+
+        public bool ContainsKey(K key)
+        {
+            return _localCache.FirstOrDefault(x => x.KeyObject.Equals(key)) != null;
+        }
+
+        public bool ContainsValue(T value)
+        {
+            return _localCache.FirstOrDefault(x => x.DataObject.Equals(value)) != null;
+        }
+
         public PersistantDictionaryElement<K, T> Get(string id, bool forceDb = false)
         {
             if (forceDb)
