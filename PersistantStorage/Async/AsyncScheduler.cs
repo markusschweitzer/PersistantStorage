ecde2ba6faeed2089162c144fbb400809013d050
Markus Schweitzer <me@markusschweitzer.at>
2016-06-25 15:51:58 +0200

    Added ASyncScheduler class.

---
+++ PersistantStorage/Async/AsyncScheduler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistantStorage.Async
+{
+    internal class AsyncScheduler
+    {
+        private readonly List<Task> _taskQue;
+
+        public AsyncScheduler()
+        {
+            _taskQue = new List<Task>();
+        }
+
+        public void AddTask(Task newTask)
+        {
+            if (_taskQue.Count == 0)
+            {
+                _taskQue.Add(newTask);
+                newTask.Start();
+            }
+            else
+            {
+                _taskQue.Add(newTask);
+                _taskQue[_taskQue.Count - 2].ContinueWith((t) => newTask.Start());
+            }
+        }
+    }
+}
